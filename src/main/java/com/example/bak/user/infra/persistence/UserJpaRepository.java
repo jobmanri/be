76f70080e9fc1a103735f4004c9795c816c392e0
BaeJunH0 <bjh14757@naver.com>
2025-11-16 03:06:09 +0900

    style : 컨벤션에 맞게 공백 추가

--- src/main/java/com/example/bak/user/infra/persistence/UserJpaRepository.java
+++ src/main/java/com/example/bak/user/infra/persistence/UserJpaRepository.java
@@ -2,12 +2,12 @@ package com.example.bak.user.infra.persistence;
 
 import com.example.bak.user.domain.User;
 import com.example.bak.user.domain.UserRepository;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface UserJpaRepository extends JpaRepository<User, Long>, UserRepository {
+    
     @Override
     List<User> findAll();
     

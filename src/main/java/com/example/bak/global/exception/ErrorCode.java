b2f169b0c4a7982c30827c4dd9e2a3914af478cb
KNU-K <knukang334@gmail.com>
2025-11-16 23:26:24 +0900

    fix : ErrorCode 내에 UserNotFound 에러 코드 추가

--- src/main/java/com/example/bak/global/exception/ErrorCode.java
+++ src/main/java/com/example/bak/global/exception/ErrorCode.java
@@ -5,17 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 /**
- * code - 도메인 별 코드 <br>
- * -> (ex - 'US001' [도메인의 N번째 에러에 해당])<br>
- * status - Http 상태 코드<br>
- * message - 클라이언트에게 응답해주는 메시지
+ * code - 도메인 별 코드 <br> -> (ex - 'US001' [도메인의 N번째 에러에 해당])<br> status - Http 상태 코드<br> message -
+ * 클라이언트에게 응답해주는 메시지
+ *
  * @author polyglot-k
  * @version 0.0.1
  */
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    ;
+    USER_NOT_FOUND("US001", HttpStatus.NOT_FOUND, "사용자 리소스를 찾을 수 없습니다.");
 
     private final String code;
     private final HttpStatus status;

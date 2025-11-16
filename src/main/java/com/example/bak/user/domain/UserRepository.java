8515ff78e656f24a3214b02f9cbeef5e7226cd6d
KNU-K <knukang334@gmail.com>
2025-11-16 23:28:53 +0900

    fix : UserRepository 내에 userId 를 통한 sub domain 인 profile 조회

--- src/main/java/com/example/bak/user/domain/UserRepository.java
+++ src/main/java/com/example/bak/user/domain/UserRepository.java
@@ -1,10 +1,13 @@
 package com.example.bak.user.domain;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository {
-    
+
     List<User> findAll();
-    
+
     User save(User user);
+
+    Optional<Profile> findProfileByUserId(Long userId);
 }

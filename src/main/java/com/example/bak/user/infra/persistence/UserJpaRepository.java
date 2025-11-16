4535390d125bfb7720c5e9f5637ddaf89b3b07e2
KNU-K <knukang334@gmail.com>
2025-11-16 23:28:53 +0900

    fix : UserRepository 내에 userId 를 통한 sub domain 인 profile 조회

--- src/main/java/com/example/bak/user/infra/persistence/UserJpaRepository.java
+++ src/main/java/com/example/bak/user/infra/persistence/UserJpaRepository.java
@@ -1,16 +1,22 @@
 package com.example.bak.user.infra.persistence;
 
+import com.example.bak.user.domain.Profile;
 import com.example.bak.user.domain.User;
 import com.example.bak.user.domain.UserRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserJpaRepository extends JpaRepository<User, Long>, UserRepository {
-    
+
     @Override
     List<User> findAll();
-    
+
     @Override
     User save(User user);
+
+    @Override
+    @Query("select u.profile from users u where u.id = :userId")
+    Optional<Profile> findProfileByUserId(Long userId);
 }

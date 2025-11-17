6730fa2f151d15c93946e670482e1ffe99c69f9c
jbh010204 <jbh010204@naver.com>
2025-11-17 19:18:35 +0900

    feat : FeedRepository 메서드 추가

--- src/main/java/com/example/bak/feed/domain/FeedRepository.java
+++ src/main/java/com/example/bak/feed/domain/FeedRepository.java
@@ -1,10 +1,17 @@
 package com.example.bak.feed.domain;
 
 import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface FeedRepository {
 
     List<Feed> findAll();
 
+    Page<Feed> findAll(Pageable pageable);
+
     Feed save(Feed feed);
+
+    Optional<Feed> findFeedById(Long id);
 }

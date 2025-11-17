76a17c7ca0f5d61709d468df275782d3284371c7
KNU-K <knukang334@gmail.com>
2025-11-18 08:05:38 +0900

    fix : UserRepository 내에 사용하지 않는 findAll() 메소드 제거

--- src/main/java/com/example/bak/user/infra/persistence/UserJpaRepository.java
+++ src/main/java/com/example/bak/user/infra/persistence/UserJpaRepository.java
@@ -3,15 +3,11 @@ package com.example.bak.user.infra.persistence;
 import com.example.bak.user.domain.Profile;
 import com.example.bak.user.domain.User;
 import com.example.bak.user.domain.UserRepository;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserJpaRepository extends JpaRepository<User, Long>, UserRepository {
-
-    @Override
-    List<User> findAll();
 
     @Override
     User save(User user);

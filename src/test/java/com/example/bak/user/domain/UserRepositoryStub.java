fdadf3f41df625e570627a563919562c8e5abf9f
KNU-K <knukang334@gmail.com>
2025-11-18 08:08:34 +0900

    test : AbstractStubRepository 기반 UserRepositoryStub 구현 - Stub 의 특성상 Id 의 생성 주도권은 테스트 코드에 있도록 처리 - Fake 와 Stub 의 형태를 고민했고, 순수 유스케이스의 로직을 테스트하기 위해서 Stub 을 선택

---
+++ src/test/java/com/example/bak/user/domain/UserRepositoryStub.java
@@ -0,0 +1,30 @@
+package com.example.bak.user.domain;
+
+import com.example.bak.global.support.AbstractStubRepository;
+import java.util.Objects;
+import java.util.Optional;
+
+public class UserRepositoryStub
+        extends AbstractStubRepository<Long, User>
+        implements UserRepository {
+
+    @Override
+    protected Long getId(User user) {
+        return user.getId();
+    }
+
+    @Override
+    protected boolean isSame(Long left, Long right) {
+        return Objects.equals(left, right);
+    }
+
+    @Override
+    public User save(User entity) {
+        return super.save(entity);
+    }
+
+    @Override
+    public Optional<Profile> findProfileByUserId(Long userId) {
+        return findById(userId).map(User::getProfile);
+    }
+}

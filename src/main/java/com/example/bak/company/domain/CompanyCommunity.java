4b4259afee861e01649c203a0a2bf06a028a7915
jbh010204 <jbh010204@naver.com>
2025-11-17 19:19:05 +0900

    refactor : CompanyCommunity와 Company 연관관계 추가

--- src/main/java/com/example/bak/company/domain/CompanyCommunity.java
+++ src/main/java/com/example/bak/company/domain/CompanyCommunity.java
@@ -2,9 +2,11 @@ package com.example.bak.company.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -26,6 +28,9 @@ public class CompanyCommunity {
     @Column(nullable = false)
     private String jobGroup;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
+    
     private CompanyCommunity(String name, String jobGroup) {
         this.name = name;
         this.jobGroup = jobGroup;

2c20c3decb6aaa2b8a875e1b3d24405c1b34b899
jbh010204 <jbh010204@naver.com>
2025-11-17 19:18:35 +0900

    feat : FeedRepository 메서드 추가

--- src/main/java/com/example/bak/feed/infra/persistence/FeedJpaRepository.java
+++ src/main/java/com/example/bak/feed/infra/persistence/FeedJpaRepository.java
@@ -3,6 +3,9 @@ package com.example.bak.feed.infra.persistence;
 import com.example.bak.feed.domain.Feed;
 import com.example.bak.feed.domain.FeedRepository;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FeedJpaRepository extends JpaRepository<Feed, Long>, FeedRepository {
@@ -11,5 +14,11 @@ public interface FeedJpaRepository extends JpaRepository<Feed, Long>, FeedReposi
     List<Feed> findAll();
 
     @Override
+    Page<Feed> findAll(Pageable pageable);
+
+    @Override
     Feed save(Feed feed);
+
+    @Override
+    Optional<Feed> findFeedById(Long id);
 }

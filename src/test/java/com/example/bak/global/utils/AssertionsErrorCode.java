f029aac475454c18557f16f3c4787bf7b875e540
KNU-K <knukang334@gmail.com>
2025-11-18 17:32:53 +0900

    test : ErrorCode 기반 AssertThrow 유틸 분리

---
+++ src/test/java/com/example/bak/global/utils/AssertionsErrorCode.java
@@ -0,0 +1,17 @@
+package com.example.bak.global.utils;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.example.bak.global.exception.BusinessException;
+import com.example.bak.global.exception.ErrorCode;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class AssertionsErrorCode {
+
+    public static void assertBusiness(Runnable runnable, ErrorCode errorCode) {
+        assertThatThrownBy(runnable::run)
+                .isInstanceOf(BusinessException.class)
+                .hasMessage(errorCode.getMessage());
+    }
+}

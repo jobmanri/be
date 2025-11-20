3fbf6a022876a47bb1af272eab7837e3c3fd7f5a
BaeJunH0 <bjh14757@naver.com>
2025-11-20 23:57:58 +0900

    feat : Result Inner 클래스로 DTO 구성

--- src/main/java/com/example/bak/community/application/dto/CommunityResult.java
+++ src/main/java/com/example/bak/community/application/dto/CommunityResult.java
@@ -2,9 +2,25 @@ package com.example.bak.community.application.dto;
 
 import com.example.bak.community.domain.Community;
 
-public record CommunityResult() {
+public class CommunityResult {
 
-    public static CommunityResult from(Community community) {
-        return new CommunityResult();
+    public record ResourcePath(Long communityId) {
+
+        public static ResourcePath from(Community community) {
+            return new ResourcePath(community.getId());
+        }
+    }
+
+    public record Detail(
+            Long id,
+            String name,
+            String jobGroup
+    ) {
+
+        public static Detail from(Community community) {
+            return new Detail(
+                    community.getId(), community.getName(), community.getJobGroup()
+            );
+        }
     }
 }

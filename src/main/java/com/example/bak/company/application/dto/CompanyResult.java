0b69c9df4d097b39d256b69c9446815d081a9726
BaeJunH0 <bjh14757@naver.com>
2025-11-20 23:23:04 +0900

    feat : Result Inner 클래스로 DTO 구성

--- src/main/java/com/example/bak/company/application/dto/CompanyResult.java
+++ src/main/java/com/example/bak/company/application/dto/CompanyResult.java
@@ -1,12 +1,37 @@
 package com.example.bak.company.application.dto;
 
 import com.example.bak.company.domain.Company;
+import java.util.List;
 
-public record CompanyResult(
+public class CompanyResult {
 
-) {
+    public record ResourcePath(
+            Long companyId
+    ) {
 
-    public static CompanyResult from(Company company) {
-        return new CompanyResult();
+        public static ResourcePath from(Company company) {
+            return new ResourcePath(company.getId());
+        }
+    }
+
+    public record Detail(
+            Long companyId,
+            String name,
+            String careerLink,
+            String logoUrl,
+            String description,
+            List<CommunityInfo> communityInfos
+    ) {
+
+        public static Detail from(Company company) {
+            return new Detail(
+                    company.getId(),
+                    company.getName(),
+                    company.getCareerLink(),
+                    company.getLogoUrl(),
+                    company.getDescription(),
+                    company.getCommunities().stream().map(CommunityInfo::from).toList()
+            );
+        }
     }
 }

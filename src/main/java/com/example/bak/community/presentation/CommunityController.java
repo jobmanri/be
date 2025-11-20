a9b127e11111ba542f72228ef36325e378bdeefe
BaeJunH0 <bjh14757@naver.com>
2025-11-20 23:59:32 +0900

    feat : CommunityController의 주석 삭제 및 반환 값 정의

--- src/main/java/com/example/bak/community/presentation/CommunityController.java
+++ src/main/java/com/example/bak/community/presentation/CommunityController.java
@@ -4,6 +4,8 @@ import com.example.bak.community.application.CommunityService;
 import com.example.bak.community.application.dto.CommunityResult;
 import com.example.bak.community.presentation.dto.CommunityRequest;
 import com.example.bak.global.common.response.ApiResponse;
+import com.example.bak.global.common.response.ApiResponseFactory;
+import com.example.bak.global.common.utils.UriUtils;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,53 +15,54 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class CommunityController {
 
     private final CommunityService communityService;
 
-    // 회사 별 커뮤니티 조회
-    @GetMapping("/{companyId}/communities")
+    @GetMapping("/company/{companyId}/communities")
     public ResponseEntity<ApiResponse> getCommunities(
             @PathVariable Long companyId
     ) {
-        List<CommunityResult> communityResults = communityService.getCompanyCommunities(companyId);
-        return null;
+        List<CommunityResult.Detail> communityResults = communityService.getCommunities(companyId);
+        ApiResponse response = ApiResponseFactory.success("커뮤니티 목록을 성공적으로 불러왔습니다.",
+                communityResults);
+        return ResponseEntity.ok(response);
     }
 
-    // 회사별 커뮤니티 등록
-    @PostMapping("/{companyId}/communities")
+    @PostMapping("/company/{companyId}/communities")
     public ResponseEntity<ApiResponse> createCommunity(
             @PathVariable Long companyId,
             @RequestBody CommunityRequest request
     ) {
-        communityService.createCompanyCommunity(companyId, request.name(), request.jobGroup());
-        return null;
+        CommunityResult.ResourcePath resourcePath = communityService.createCommunity(
+                companyId, request.name(), request.jobGroup());
+        ApiResponse response = ApiResponseFactory.successVoid("커뮤니티 정보를 성공적으로 저장했습니다.");
+        return ResponseEntity.created(UriUtils.current(resourcePath.communityId()))
+                .body(response);
     }
 
-    // 회사 커뮤니티 정보 수정
-    @PutMapping("/{companyId}/communities/{communityId}")
+    @PutMapping("/communities/{communityId}")
     public ResponseEntity<ApiResponse> updateCompanyCommunity(
-            @PathVariable Long companyId,
             @PathVariable Long communityId,
             @RequestBody CommunityRequest request
     ) {
-        communityService.updateCompanyCommunity(
-                companyId, communityId, request.name(), request.jobGroup()
-        );
-        return null;
+        communityService.updateCommunity(communityId, request.name(), request.jobGroup());
+        ApiResponse response = ApiResponseFactory.successVoid("커뮤니티 정보를 성공적으로 수정했습니다.");
+        return ResponseEntity.ok(response);
     }
 
-    // 회사 커뮤니티 삭제
-    @DeleteMapping("/{companyId}/communities/{communityId}")
+    @DeleteMapping("/communities/{communityId}")
     public ResponseEntity<ApiResponse> deleteCommunityCompany(
-            @PathVariable Long companyId,
             @PathVariable Long communityId
     ) {
-        communityService.deleteCompanyCommunity(companyId, communityId);
-        return null;
+        communityService.deleteCommunity(communityId);
+        ApiResponse response = ApiResponseFactory.successVoid("회사 정보를 성공적으로 삭제했습니다.");
+        return ResponseEntity.ok(response);
     }
 }

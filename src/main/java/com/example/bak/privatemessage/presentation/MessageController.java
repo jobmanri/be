69859fa2cafa1862331810027150d5b9f2272470
KNU-K <knukang334@gmail.com>
2025-11-22 06:14:03 +0900

    feat : Query 와 Command Service를 사용해서 MessageController 구현

---
+++ src/main/java/com/example/bak/privatemessage/presentation/MessageController.java
@@ -0,0 +1,84 @@
+package com.example.bak.privatemessage.presentation;
+
+import com.example.bak.global.common.response.ApiResponse;
+import com.example.bak.global.common.response.ApiResponseFactory;
+import com.example.bak.privatemessage.application.command.MessageCommandService;
+import com.example.bak.privatemessage.application.query.MessageQueryService;
+import com.example.bak.privatemessage.application.query.dto.MessageItemResult;
+import com.example.bak.privatemessage.application.query.dto.MessagePartnerResult;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/private-messages")
+@RequiredArgsConstructor
+public class MessageController {
+
+    private final MessageCommandService commandService;
+    private final MessageQueryService queryService;
+
+    @PostMapping()
+    public ResponseEntity<ApiResponse> sendMessage(
+            @RequestBody MessageRequest request
+    ) {
+        commandService.sendMessage(
+                request.senderId(),
+                request.receiverId(),
+                request.content()
+        );
+
+        return ResponseEntity.ok(
+                ApiResponseFactory.successVoid("쪽지를 성공적으로 전송했습니다.")
+        );
+    }
+
+    @GetMapping("/participants")
+    public ResponseEntity<ApiResponse> getParticipants(@RequestParam Long userId) {
+        List<MessagePartnerResult> result = queryService.getPartnersByUserId(userId);
+        return ResponseEntity.ok(ApiResponseFactory.success("쪽지 대화 상대 목록을 조회하였습니다.", result));
+    }
+
+    @GetMapping()
+    public ResponseEntity<ApiResponse> getMessagesBetweenUsers(
+            @RequestParam Long userId,
+            @RequestParam Long participantId
+    ) {
+        List<MessageItemResult> result = queryService.getMessagesBetweenUsers(
+                userId,
+                participantId
+        );
+        return ResponseEntity.ok(ApiResponseFactory.success("쪽지 내용들을 조회하였습니다.", result));
+    }
+
+    @PatchMapping("/read")
+    public ResponseEntity<ApiResponse> markAsRead(
+            @RequestParam Long userId,
+            @RequestParam Long participantId
+    ) {
+        commandService.markAsRead(userId, participantId);
+        return ResponseEntity.ok(
+                ApiResponseFactory.successVoid("읽음 처리되었습니다.")
+        );
+    }
+
+    @DeleteMapping("/{messageId}")
+    public ResponseEntity<ApiResponse> deleteMessage(
+            @RequestParam Long userId,
+            @PathVariable Long messageId
+    ) {
+        commandService.deleteMessage(userId, messageId);
+        return ResponseEntity.ok(
+                ApiResponseFactory.successVoid("쪽지를 삭제했습니다.")
+        );
+    }
+}

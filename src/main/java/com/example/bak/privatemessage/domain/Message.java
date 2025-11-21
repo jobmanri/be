82c17b45c24705f25259f7f96e8d516ca8e7fc59
KNU-K <knukang334@gmail.com>
2025-11-22 06:06:07 +0900

    feat : Message 도메인 엔티티 설계 및 VO 추가 - Message, MessageParticipants, MessageState 등의 형태로 분리하여 관리 되도록 처리 - VO 내부 값이 모두 null일 때 초기화 되지않아서 NPE 뜨는 문제로 PostLoad 사용

---
+++ src/main/java/com/example/bak/privatemessage/domain/Message.java
@@ -0,0 +1,59 @@
+package com.example.bak.privatemessage.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PostLoad;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "private_messages")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private MessageParticipants messageParticipants;
+
+    @Embedded
+    private MessageState messageState;
+
+    @Column(nullable = false)
+    private String content;
+
+    private Message(
+            MessageParticipants messageParticipants,
+            String content
+    ) {
+        this.messageParticipants = messageParticipants;
+        this.content = content;
+    }
+
+    public static Message create(
+            Long senderId,
+            Long receiverId,
+            String content
+    ) {
+        MessageParticipants participants = MessageParticipants.of(senderId, receiverId);
+        Message message = new Message(participants, content);
+        message.messageState = MessageState.init();
+        return message;
+    }
+
+    @PostLoad
+    private void postLoad() {
+        if (this.messageState == null) {
+            this.messageState = MessageState.init();
+        }
+    }
+}

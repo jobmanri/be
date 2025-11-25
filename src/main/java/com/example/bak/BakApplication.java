18e56d0dc581c8810bf3fce6d95b19129fd1b4bd
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-11-26 00:23:52 +0900

    Feat: PropertiesScan 어노테이션 삭제 (#19)

--- src/main/java/com/example/bak/BakApplication.java
+++ src/main/java/com/example/bak/BakApplication.java
@@ -1,15 +1,23 @@
 package com.example.bak;
 
+import com.example.bak.auth.infra.jwt.persistence.JwtProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-
+@Slf4j
+@RequiredArgsConstructor
 @SpringBootApplication
-@ConfigurationPropertiesScan
-public class BakApplication {
-
+public class BakApplication implements CommandLineRunner {
+    private final JwtProperties properties;
     public static void main(String[] args) {
         SpringApplication.run(BakApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("access: {}",properties.getAccess());
+
+    }
 }

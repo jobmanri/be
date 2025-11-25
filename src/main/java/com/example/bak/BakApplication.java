3ca1f7d5a65ccbf212f61c73104295d8bccb4cc2
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-11-26 00:32:16 +0900

    Fix: application.yaml 삭제 및 CommandLineRunner 삭제 (#19)

--- src/main/java/com/example/bak/BakApplication.java
+++ src/main/java/com/example/bak/BakApplication.java
@@ -1,23 +1,11 @@
 package com.example.bak;
 
-import com.example.bak.auth.infra.jwt.persistence.JwtProperties;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@Slf4j
-@RequiredArgsConstructor
+
 @SpringBootApplication
-public class BakApplication implements CommandLineRunner {
-    private final JwtProperties properties;
+public class BakApplication {
     public static void main(String[] args) {
         SpringApplication.run(BakApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        log.info("access: {}",properties.getAccess());
-
     }
 }

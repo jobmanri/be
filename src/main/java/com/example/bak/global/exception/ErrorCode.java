96193adce80fceb1ac4ccfc9d33376856e1765bc
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-11-25 23:20:07 +0900

    Feat: 인증 관련 ErrorCode 추가 (#19) - 토큰이 없는 경우 - 토큰이 부적절한 경우 - 원하는 Method 를 실행할 권한이 없는 경우 - Token 을 분해했지만, 정보가 없는 경우

--- src/main/java/com/example/bak/global/exception/ErrorCode.java
+++ src/main/java/com/example/bak/global/exception/ErrorCode.java
@@ -14,6 +14,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+    // Auth
+    INCORRECT_PASSWORD("AU001", HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다"),
+    INVALID_TOKEN("AU002", HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않습니다"),
+    NO_ATTRIBUTES("AU003", HttpStatus.BAD_REQUEST, "토큰 검증에 필요한 정보가 없습니다."),
+    NO_TOKEN("AU004", HttpStatus.UNAUTHORIZED, "토큰이 없습니다."),
+    NO_AUTHORITY("AU005", HttpStatus.FORBIDDEN, "해당 정보에 접근할 수 있는 권한이 없습니다."),
     // Company
     COMPANY_NOT_FOUND("CM001", HttpStatus.NOT_FOUND, "회사 리소스를 찾을 수 없습니다."),
     COMPANY_COMMUNITY_NOT_FOUND("CC001", HttpStatus.NOT_FOUND, "커뮤니티 리소스를 찾을 수 없습니다."),

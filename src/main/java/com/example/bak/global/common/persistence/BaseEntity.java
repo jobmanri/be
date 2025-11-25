1d3587600799a2d5c4c28ea8e4c169be47c89bbc
KNU-K <knukang334@gmail.com>
2025-11-25 18:45:30 +0900

    refactor : BaseEntity 내에 Getter 및 AuditingEntityListener 추가

--- src/main/java/com/example/bak/global/common/persistence/BaseEntity.java
+++ src/main/java/com/example/bak/global/common/persistence/BaseEntity.java
@@ -1,11 +1,16 @@
 package com.example.bak.global.common.persistence;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @CreatedDate

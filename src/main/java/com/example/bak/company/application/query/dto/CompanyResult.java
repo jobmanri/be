f0c608f09fa56c5a61b26c0c8baf06e5c8ff404a
BaeJunH0 <bjh14757@naver.com>
2025-11-26 00:19:09 +0900

    feat : query 디렉토리로 이동 및 평탄화
    기존 방식으로 Query 결과를 담으려면 Json 처리가 필수적, 이에 따라 평탄화 이후 원래 DTO로 변환하는 과정 추가

---
+++ src/main/java/com/example/bak/company/application/query/dto/CompanyResult.java
@@ -0,0 +1,53 @@
+package com.example.bak.company.application.query.dto;
+
+import com.example.bak.community.application.dto.CommunityResult;
+import com.example.bak.company.domain.Company;
+import java.util.List;
+
+public class CompanyResult {
+
+    public record CompanyId(
+            Long value
+    ) {
+
+        public static CompanyId from(Company company) {
+            return new CompanyId(company.getId());
+        }
+    }
+
+    public record Detail(
+            Long companyId,
+            String name,
+            String careerLink,
+            String logoUrl,
+            String description,
+            List<CommunityResult.Detail> communityInfos
+    ) {
+        public static Detail from(List<Flat> flats) {
+            Flat first = flats.getFirst();
+
+            return new Detail(
+                    first.companyId(),
+                    first.communityName(),
+                    first.careerLink(),
+                    first.logoUrl(),
+                    first.description(),
+                    flats.stream()
+                            .map(CommunityResult.Detail::from)
+                            .toList()
+            );
+        }
+    }
+
+    public record Flat(
+            Long companyId,
+            String name,
+            String careerLink,
+            String logoUrl,
+            String description,
+            Long communityId,
+            String communityName,
+            String jobGroup
+    ) {
+    }
+}

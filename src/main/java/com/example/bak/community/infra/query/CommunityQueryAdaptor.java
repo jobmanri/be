ed4d295dd4f01bfa45450bdd84b27bf4b86d8fcf
BaeJunH0 <bjh14757@naver.com>
2025-11-26 21:45:29 +0900

    feat : communityJdbcRepository 사용하도록 설정

--- src/main/java/com/example/bak/community/infra/query/CommunityQueryAdaptor.java
+++ src/main/java/com/example/bak/community/infra/query/CommunityQueryAdaptor.java
@@ -3,6 +3,7 @@ package com.example.bak.community.infra.query;
 
 import com.example.bak.community.application.query.dto.CommunityResult;
 import com.example.bak.community.application.query.port.CommunityQueryPort;
+import com.example.bak.community.infra.query.jdbc.CommunityJdbcRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -11,9 +12,10 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class CommunityQueryAdaptor implements CommunityQueryPort {
 
+    private final CommunityJdbcRepository communityJdbcRepository;
 
     @Override
-    public List<CommunityResult.Detail> findByCompanyId(Long id) {
-        return List.of();
+    public List<CommunityResult.Detail> findByCompanyId(Long companyId) {
+        return communityJdbcRepository.findByCompanyId(companyId);
     }
 }

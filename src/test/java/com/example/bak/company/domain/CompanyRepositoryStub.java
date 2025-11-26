5c3e304fb6860a47adbe951f65ed46b0c1f964f2
BaeJunH0 <bjh14757@naver.com>
2025-11-26 14:18:27 +0900

    refactor : Repository 구현체에 따르도록 변경

--- src/test/java/com/example/bak/company/domain/CompanyRepositoryStub.java
+++ src/test/java/com/example/bak/company/domain/CompanyRepositoryStub.java
@@ -1,12 +1,13 @@
 package com.example.bak.company.domain;
 
 
+import com.example.bak.company.application.command.port.CompanyCommandPort;
 import com.example.bak.global.support.AbstractStubRepository;
 import java.util.Objects;
 
 public class CompanyRepositoryStub
         extends AbstractStubRepository<Long, Company>
-        implements CompanyRepository {
+        implements CompanyCommandPort {
 
     @Override
     protected Long getId(Company company) {

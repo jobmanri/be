c0f1529a24b648696d31e1e6995382540c739d5c
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-11-27 09:23:48 +0900

    fix: 내부 로직 Method 분리

--- src/main/java/com/example/bak/global/security/filter/JwtTokenRequestFilter.java
+++ src/main/java/com/example/bak/global/security/filter/JwtTokenRequestFilter.java
@@ -1,10 +1,10 @@
 package com.example.bak.global.security.filter;
 
+import com.example.bak.auth.domain.TokenType;
 import com.example.bak.auth.infra.jwt.JwtValidator;
-import com.example.bak.auth.infra.jwt.persistence.TokenType;
+import com.example.bak.auth.infra.jwt.utils.JwtUtils;
 import com.example.bak.global.exception.ErrorCode;
 import com.example.bak.global.security.dto.AuthInfo;
-import com.example.bak.auth.infra.jwt.utils.JwtUtils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
@@ -12,47 +12,47 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.util.PatternMatchUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-
 @Component
+@RequiredArgsConstructor
 public class JwtTokenRequestFilter extends OncePerRequestFilter {
-    private static final String[] whiteList = {"/api/v1/users", "/api/v1/auth/login", "/css/*"};
+
+    private static final String[] WHITE_LIST = {"/api/v1/users", "/api/v1/auth/login", "/css/*"};
     private final JwtValidator jwtValidator;
 
-    public JwtTokenRequestFilter(JwtValidator jwtValidator) {
-        this.jwtValidator = jwtValidator;
-    }
-
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String requestURL = request.getRequestURI();
-        if(isWhiteList(requestURL)) {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        String token = JwtUtils.resolveToken(request.getHeader(HttpHeaders.AUTHORIZATION))
+                .orElse(null);
+
+        if (filterValidate(request, token)) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        String token = JwtUtils.resolveToken(request.getHeader(HttpHeaders.AUTHORIZATION));
-        if(token == null){
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        try{
+        try {
             Jws<Claims> claims = jwtValidator.validate(TokenType.ACCESS, token);
             request.setAttribute(JwtUtils.CLAIMS_KEY, AuthInfo.from(claims));
-        }catch(JwtException e){
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ErrorCode.INVALID_TOKEN.getMessage());
-            return;
+            filterChain.doFilter(request, response);
+        } catch (JwtException e) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                    ErrorCode.INVALID_TOKEN.getMessage());
         }
-        filterChain.doFilter(request, response);
+    }
+
+    private boolean filterValidate(HttpServletRequest request, String token) {
+        return isWhiteList(request.getRequestURI()) || token == null;
     }
 
     private boolean isWhiteList(String requestURL) {
-        return PatternMatchUtils.simpleMatch(whiteList, requestURL);
+        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURL);
     }
 }

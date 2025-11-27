858b4ca526970ffdcfea12d13f711ad12e3744a6
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-11-27 09:18:38 +0900

    fix: DateUtils 에서 진행하던 ttl 에 대한 예외처리를 Utils 에 책임부여했던 것을 Fix (#19)

--- src/main/java/com/example/bak/global/common/utils/DateUtils.java
+++ src/main/java/com/example/bak/global/common/utils/DateUtils.java
@@ -1,14 +1,14 @@
 package com.example.bak.global.common.utils;
 
-import lombok.experimental.UtilityClass;
-
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Date;
+import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class DateUtils {
+
     public final static ZoneId zoneId = ZoneId.of("Asia/Seoul");
 
     public static Date getNow() {
@@ -17,10 +17,7 @@ public class DateUtils {
         return Date.from(now.toInstant());
     }
 
-    public static Date getExpirationDate(Long ttl){
-        if(ttl == null){
-            return null;
-        }
+    public static Date getExpirationDate(Long ttl) {
         Instant nowUtc = Instant.now().plusMillis(ttl);
         ZonedDateTime now = nowUtc.atZone(zoneId);
         return Date.from(now.toInstant());

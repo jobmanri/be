5dd26e1c0ec9693943937a7ef2470bc4a2153870
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-11-27 09:21:34 +0900

    Fix: JwtUtils 내부 로직 변경 (#19) - Optional 객체 사용 - 전달받은 Param 값 null 여부 확인 로직 수정

--- src/main/java/com/example/bak/auth/infra/jwt/utils/JwtUtils.java
+++ src/main/java/com/example/bak/auth/infra/jwt/utils/JwtUtils.java
@@ -1,24 +1,26 @@
 package com.example.bak.auth.infra.jwt.utils;
 
-import org.springframework.util.StringUtils;
+import java.util.Optional;
 
 public class JwtUtils {
+
     public final static String PREFIX_BEARER_TOKEN = "Bearer ";
     public final static String CLAIMS_KEY = "claims";
 
     /**
      * HttpServletRequest의 Authorization 헤더 값에서 실제 JWT 토큰 문자열을 추출합니다.
      * <p>
-     * 입력된 값이 비어있지 않고 'Bearer '로 시작하는 경우, 접두사를 제외한 실제 토큰 값을 반환합니다.
-     * 조건에 맞지 않는 경우 {@code null}을 반환합니다.
+     * 입력된 값이 비어있지 않고 'Bearer '로 시작하는 경우, 접두사를 제외한 실제 토큰 값을 반환합니다. 조건에 맞지 않는 경우 {@code null}을
+     * 반환합니다.
      *
      * @param bearerToken Authorization 헤더에서 전달된 Bearer 토큰 문자열
      * @return 'Bearer ' 접두사가 제거된 순수 토큰 문자열, 또는 유효하지 않은 경우 {@code null}
      */
-    public static String resolveToken(String bearerToken) {
-        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(PREFIX_BEARER_TOKEN)) {
-            return bearerToken.substring(PREFIX_BEARER_TOKEN.length());
+    public static Optional<String> resolveToken(String bearerToken) {
+        if (bearerToken == null || !bearerToken.startsWith(PREFIX_BEARER_TOKEN)) {
+            return Optional.empty();
         }
-        return null;
+        String token = bearerToken.substring(PREFIX_BEARER_TOKEN.length());
+        return Optional.of(token);
     }
 }

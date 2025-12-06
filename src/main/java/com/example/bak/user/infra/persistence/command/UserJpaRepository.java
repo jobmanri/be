f12d6583b48e647c6162f80d61e2e9df15c0e406
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-12-06 23:05:27 +0900

    feat: UserJpaRepository modified for CQRS (#36)

--- src/main/java/com/example/bak/user/infra/persistence/command/UserJpaRepository.java
+++ src/main/java/com/example/bak/user/infra/persistence/command/UserJpaRepository.java
@@ -1,11 +1,9 @@
-package com.example.bak.user.infra.persistence;
+package com.example.bak.user.infra.persistence.command;
 
-import com.example.bak.user.domain.Profile;
 import com.example.bak.user.domain.User;
 import com.example.bak.user.domain.UserRepository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserJpaRepository extends JpaRepository<User, Long>, UserRepository {
 
@@ -13,9 +11,8 @@ public interface UserJpaRepository extends JpaRepository<User, Long>, UserReposi
     User save(User user);
 
     @Override
-    @Query("select u.profile from users u where u.id = :userId")
-    Optional<Profile> findProfileByUserId(Long userId);
+    Optional<User> findById(Long id);
 
     @Override
-    Optional<User> findById(Long id);
+    Optional<User> findByEmail(String email);
 }
